db368894c945ec541a8d1477deacf23692e58052
Virgil <125411180+vyamat20@users.noreply.github.com>
2023-05-09 20:31:56 -0700

    Updated javascript.js created function that plays one round of rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -9,3 +9,23 @@ function playerSelection()  {
    return selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
 };
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+    console.log("It's a tie! You both picked rock")}
+    else if (playerSelection == "Paper" && computerSelection == "Paper") {
+    console.log("It's a tie! You both picked paper")}
+    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+    console.log("It's a tie! You both picked scissors")}
+    else if (playerSelection == "Rock"  && computerSelection == "Scissors") {
+    console.log("You Win! Rock beats Scissor")}
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    console.log("You lose! Paper beats rock")}
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    console.log("You lose! Scissors beats paper")}
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    console.log("You win! Paper beats rock")}
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    console.log("You win! Scissors beats paper")}
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    console.log("You lose! Rock beats Scissorts")}
+    }
